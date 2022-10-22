62352c557ab8f2c150fd93cf92c62dcce9d79f1c
experimentproduction <endornp@gmail.com>
2022-10-22 14:27:47 +0700

    Project Expe

---
+++ java/for.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+ 
+    for (int c=700; c>= 60; c-=5){
+        cout <<c<<endl;
+    }
+   
+    
+}
---
+++ java/main1.java
@@ -0,0 +1,20 @@
+// package com.turorial
+
+// public class public static void main(String[] args) {
+//     system.out.println("hello endor"),
+    
+// }
+
+// public class main {
+//     public static void main (string[] args) {
+//         system.out.println("hellow endor"),
+//     }
+// }
+
+public class main{
+    public static void main(String[] args){
+        system.out.println("hello expe");
+
+    }
+
+}
